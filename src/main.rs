a4e3f5f476cf083a8f03499ceef478dd9e948623
ThEMan2309 <theman2309@proton.me>
2026-02-09 11:47:19 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@
 use std::io::{self, Write};
 
 const EXIT_CMD: &str = "exit";
-const ECHO_CMD: &str = "echo ";
+const ECHO_CMD: &str = "echo";
+const TYPE_CMD: &str = "type";
+
+const SHELL_BUILTINS: &[&str] = &[EXIT_CMD, ECHO_CMD, TYPE_CMD];
+
 
 fn repl_loop() {
     loop {
@@ -21,9 +25,19 @@ fn repl_loop() {
         }
 
         // Echo command.
-        if command.starts_with(ECHO_CMD) {
-            let arguments = &command[5..];
+        if command.starts_with(&*format!("{} ", &ECHO_CMD)) {
+            let arguments = &command[(ECHO_CMD.len() + 1)..];
             println!("{}", arguments);
+            continue;
+        }
+
+        if command.starts_with(&*format!("{} ", &TYPE_CMD)) {
+            let arguments = &command[(TYPE_CMD.len() + 1)..];
+            if SHELL_BUILTINS.contains(&arguments) {
+                println!("{} is a shell builtin", arguments);
+            } else {
+                println!("{}: not found", arguments);
+            }
             continue;
         }
 
