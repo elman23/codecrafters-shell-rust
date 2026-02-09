d34c86e07412956b48755587cc8cad7232b7873e
ThEMan2309 <theman2309@proton.me>
2026-02-09 11:18:20 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Prompt
-    print!("$ ");
-    io::stdout().flush().unwrap();
-    
-    // Wait for user input
-    let mut command: String = String::new();
-    io::stdin().read_line(&mut command).unwrap();
-    println!("{}: command not found", command.trim());
+    // Shell's infinite loop.
+    loop {
+        // Display prompt.
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        
+        // Wait for user input.
+        let mut command: String = String::new();
+        io::stdin().read_line(&mut command).unwrap();
+        println!("{}: command not found", command.trim());
+    }
 }
