c8e5afc1af7e4e217108e9e288a5889fc3d3c2c4
ThEMan2309 <theman2309@proton.me>
2026-02-09 11:11:54 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,12 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
+    // Prompt
     print!("$ ");
     io::stdout().flush().unwrap();
+    
+    // Wait for user input
+    let mut command: String = String::new();
+    io::stdin().read_line(&mut command).unwrap();
+    println!("{}: command not found", command.trim());
 }
