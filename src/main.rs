9b7d4ee1bc30ec7cdd3f2770678563b62ec35b04
ThEMan2309 <theman2309@proton.me>
2026-02-09 18:42:36 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::fs;
 use std::env;
 use std::path::PathBuf;
 use std::os::unix::fs::PermissionsExt;
+use std::process::Command;
 
 const EXIT_CMD: &str = "exit";
 const ECHO_CMD: &str = "echo";
@@ -55,7 +56,7 @@ fn repl_loop() {
             continue;
         }
 
-        println!("{}: command not found", command);
+        exec_command(&command);
     }
 }
 
@@ -83,6 +84,56 @@ fn check_type(command: &str) {
                     if filename.split(".").next() == Some(command) && is_executable(&path_as_string) {
                         println!("{} is {}", command, path_as_string);
                         found = true;
+                        break;
+                    }
+                }
+                if found {
+                    break;
+                }
+            }
+            if !found {
+                println!("{}: command not found", command);
+            }                
+        },
+        Err(e) => {
+            println!("Couldn't read PATH: {}", e);
+        },
+    }
+}
+
+fn exec_command(command: &str) {
+    match env::var("PATH") {
+        Ok(path) => {
+            let dirs = path.split(":");
+            let mut found = false;
+            for dir in dirs {
+                let entries: Vec<PathBuf> = fs::read_dir(dir)
+                    .unwrap()
+                    .map(|res| res.map(|e| e.path()))
+                    .collect::<Result<Vec<_>, io::Error>>()
+                    .unwrap();
+
+                let mut entries: Vec<PathBuf> = entries;
+                entries.sort();
+
+                for entry in entries {
+                    let path_as_string = entry.to_string_lossy();
+                    let filename = entry
+                        .file_name()
+                        .and_then(|s| s.to_str())
+                        .unwrap_or("");
+                    let executable = command.split_whitespace().next().expect("");
+                    if filename.split(".").next() == Some(executable) && is_executable(&path_as_string) {
+                        found = true;
+                        let mut command_split = command.split_whitespace();
+                        let command_name = command_split.next().unwrap_or("");
+                        let command_args: Vec<_> = command_split.collect();
+                        Command::new(command_name)
+                            .args(command_args)
+                            .spawn()
+                            .expect("Command failed to start")
+                            .wait_with_output()
+                            .expect("Failed to wait on command");
                         break;
                     }
                 }
