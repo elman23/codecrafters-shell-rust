e2e00f212d3ea135c1ab6b92c1f7aa31333d8fc0
ThEMan2309 <theman2309@proton.me>
2026-02-10 05:24:56 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ const PWD_CMD: &str = "pwd";
 const SHELL_BUILTINS: &[&str] = &[EXIT_CMD, ECHO_CMD, TYPE_CMD, PWD_CMD];
 
 fn print_pwd() {
-    println!("{}", env::current_dir().unwrap().to_str());
+    println!("{}", env::current_dir().unwrap().to_str().unwrap());
 }
 
 fn is_executable(path: &std::path::Path) -> std::io::Result<bool> {
