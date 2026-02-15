31d1ff77844af9a670be3bc1433ce4c4186b57e1
ThEMan2309 <theman2309@proton.me>
2026-02-15 11:32:52 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -268,7 +268,7 @@ fn exec_command(command: &str) {
                     args.split_whitespace().map(|s| s.to_string()).collect()
                 };
 
-                for let mut arg in &args {
+                for mut arg in &args {
                     arg = &arg.replace("\\", "");
                 }
                 // println!("Command: {}", name);
